075952feb16b63d557588fbb2c5fae65caf33cf2
topbestaitools-droid <topbestaitools@gmail.com>
2026-03-30 17:05:28 +0200

    Create affiliate performance metrics dashboard

---
+++ app/dashboard/affiliate/page.tsx
@@ -0,0 +1,14 @@
+// affiliate/page.tsx
+
+import React from 'react';
+
+const AffiliateDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Affiliate Performance Metrics</h1>
+            {/* Analytics dashboard content will go here */}
+        </div>
+    );
+};
+
+export default AffiliateDashboard;
